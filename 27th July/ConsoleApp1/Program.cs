6657ead5a22e136718e83ef6ebb368406e8c3c74
Kinjal Samrit <kinjals.0212@gmail.com>
2022-07-28 10:31:32 +0530

    Task Submission

---
+++ 27th July/ConsoleApp1/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //string name = "Kinjal";
+            //Console.WriteLine(name);
+            //int myNum = 15;
+            //Console.WriteLine(myNum);
+            //myNum = 20;
+            //Console.WriteLine(myNum);
+            //const int myNum = 15;
+            //myNum = 20;
+            //Console.WriteLine(myNum);
+            //int myNum = 5;
+            //double myDoubleNum = 5.99D;
+            //char myLetter = 'D';
+            //bool myBool = true;
+            //string myText = "Hie";
+            //Console.WriteLine(myNum);
+            //Console.WriteLine(myText);
+            //Console.WriteLine(myBool);
+            //Console.WriteLine(myLetter);
+            //Console.WriteLine(myDoubleNum);
+            //string name = "Kinjal";
+            //Console.WriteLine("Hie " + name);
+            //string firstName = "Kinjal ";
+            //string lastName = "Samrit";
+            //string fullName = firstName + lastName;
+            //Console.WriteLine(fullName);
+            //int x = 5;
+            //int y = 6, z = 4;
+            //Console.WriteLine(x + y + z);
+            //Console.WriteLine("Enter Number: ");
+            //int num= Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("The number is " + num);
+            int x = 5;
+            x &= 3;
+            Console.WriteLine(x);
+        }
+    }
+}
---
+++ 27th July/task28thJuly/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace task28thJuly
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            EnterName ename = new EnterName();
+            ename.banned += WarningAlarm;
+            ename.User();
+            Console.Read();
+        }
+        static void WarningAlarm(object sender, BannedUserEventArgs e)
+        {
+            Console.WriteLine("{0} Users Found. Sending Email to Administration.", e.Name);
+            Console.WriteLine("Email Sent to the administrator.");
+            Console.WriteLine("Warning!!");
+            //for the sound
+            for(; ; )
+            {
+                Console.Beep();
+            }
+        }
+    }
+
+    public class EnterName
+    {
+        public event EventHandler<BannedUserEventArgs> banned;
+        public void User()
+        {
+            Console.Write("Enter You Name : ");
+            string user = Console.ReadLine();
+
+            if ((user == "Ram" || user == "Sham" || user == "Naveen") && (banned != null))
+            {
+                banned(this, new BannedUserEventArgs(user));
+            }
+            else
+            {
+                Console.WriteLine("Welcome to  " + user);
+            }
+        }
+    }
+
+    public class BannedUserEventArgs : EventArgs
+    {
+        public BannedUserEventArgs(string a)
+        {
+            Name = a;
+        }
+        public string Name { get; set; }
+    }
+}
